b5edb8ffd17904c1b0a7d95c3efa79c8755b6fb8
Sylvana Santos <sylvana.e.santos@gmail.com>
2020-02-03 15:00:41 -0500

    feat: finalizzed css styling

--- client/components/ProjectsItem.js
+++ client/components/ProjectsItem.js
@@ -3,25 +3,31 @@ import React from 'react';
 const ProjectsItem = (props) => {
     const projectStyle = {
         backgroundImage: `linear-gradient(to bottom, ${props.project.color}), url(${props.project.image})`,
-        backgroundSize: '40% 100%',
+        backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'left top'
+        backgroundPosition: 'center',
+        width: '40%',
+        height: '100%',
+        borderRadius: '4px 0 0 4px',
     }
     return (
-        <div className='project-item' style={projectStyle}>
+        <div className='project-item'>
             <div className='project-content'>
-                <div className='project-title-container'>
-                    <p className='project-title'>{props.project.name}</p>
+                <div style={projectStyle}/>
+                <div className='project-description'>
+                    <div className='project-title-container'>
+                        <p className='project-title'>{props.project.name}</p>
+                    </div>
+                    <p className='project-text'>Description: {props.project.description}</p>
+                    <p className='project-text'>Contributions:</p>
+                    <ul className='project-text'>
+                        {
+                            props.project.contributions.map((contribution, index) => <li key={index}>{contribution}</li>)
+                        }
+                    </ul>
+                    <p className='project-text'>Technologies: {props.project.technologies}</p>
+                    <a className='project-link' href={props.project.link}>View</a>
                 </div>
-                <p className='project-text'>Description: {props.project.description}</p>
-                <p className='project-text'>Contributions:</p>
-                <ul className='project-text'>
-                    {
-                        props.project.contributions.map((contribution, index) => <li key={index}>{contribution}</li>)
-                    }
-                </ul>
-                <p className='project-text'>Technologies: {props.project.technologies}</p>
-                <a className='project-link' href={props.project.link}>View</a>
             </div>
         </div>
     )
